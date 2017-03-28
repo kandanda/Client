93a057687dd41ae8b9a6045edc5e1e40e17e2402
kasacka <joel.egger@hsr.ch>
2017-03-28 17:02:00 +0200

    * Database Data Seed (Mockup) * List Tournaments #review

--- Kandanda/Kandanda.Ui/ViewModels/ControlPanelViewModel.cs
+++ Kandanda/Kandanda.Ui/ViewModels/ControlPanelViewModel.cs
@@ -1,5 +1,9 @@
-﻿using Prism.Commands;
+﻿using System.Collections.Generic;
+using Prism.Commands;
 using System.Windows.Input;
+using Kandanda.BusinessLayer.ServiceImplementations;
+using Kandanda.BusinessLayer.ServiceInterfaces;
+using Kandanda.Dal.DataTransferObjects;
 using Kandanda.Ui.Core;
 using Prism.Regions;
 
@@ -8,12 +12,16 @@ namespace Kandanda.Ui.ViewModels
     public class ControlPanelViewModel : ViewModelBase
     {
         private readonly IRegionManager _regionManager;
+        private readonly ITournamentService _tournamentService;
         public ICommand CreateTournamentCommand { get; }
-
-        public ControlPanelViewModel(IRegionManager regionManager)
+        public List<Tournament> Tournaments { get; set; }
+        
+        public ControlPanelViewModel(IRegionManager regionManager, ITournamentService tournamentService)
         {
             _regionManager = regionManager;
+            _tournamentService = tournamentService;
             CreateTournamentCommand = new DelegateCommand(NavigateToNewTournament);
+            Tournaments = tournamentService.GetAllTournaments();
         }
 
         private void NavigateToNewTournament()
