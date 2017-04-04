e2f756522a4aab11088d717b1b69059a7bdc464b
Janic Mikes <janic.mikes@hsr.ch>
2017-04-04 10:51:37 +0200

    add Teams management * change DB initializer to create full teams EPJ-77 #time 1h add teams management

--- Kandanda/Kandanda.Ui/ViewModels/TeamsViewModel.cs
+++ Kandanda/Kandanda.Ui/ViewModels/TeamsViewModel.cs
@@ -1,40 +1,50 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Windows.Input;
 using Kandanda.BusinessLayer;
 using Kandanda.BusinessLayer.ServiceInterfaces;
 using Kandanda.Ui.Core;
 using Kandanda.Dal.DataTransferObjects;
+using Prism.Commands;
 
 namespace Kandanda.Ui.ViewModels
 {
+    //TODO: Refactor to ParticipantViewModel
     public class TeamsViewModel : ViewModelBase
     {
         private IParticipantService _participantService;
+
+        public ICommand SaveCommand { get; }
+
         public ObservableCollection<Participant> Participants { get; } = new ObservableCollection<Participant>();
         public TeamsViewModel(ServiceFactory service)
         {
             _participantService = service.CreateParticipantService();
             Title = "Teams";
             PullParticipants();
+            SaveCommand = new DelegateCommand(SaveTeams, CanSaveTeams)
+                .ObservesProperty(() => Participants);
         }
         private void PullParticipants()
         {
             Participants.Clear();
-            Participants.Add(new Participant()
-            {
-                Captain = "Mike",
-                Email = "mikes@mail.com",
-                Id = 1,
-                Name = "Supersoccers",
-                Phone = "0799618760",
-                RowVersion = null
-            });
-            /*
             foreach (var participant in _participantService.GetAllParticipants())
             {
                 Participants.Add(participant);
             }
-            */
+        }
+        private void SaveTeams()
+        {
+            foreach (Participant participant in Participants)
+            {
+                _participantService.Update(participant);
+            }
+        }
+
+        private bool CanSaveTeams()
+        {
+            return true;
         }
     }
 }
