fdc122655bca291ce1ca798a2ad6846481b38c5b
kasacka <joel.egger@hsr.ch>
2017-04-28 11:30:36 +0200

    added schedule data to data layer

--- Kandanda/Kandanda.Dal/Entities/Tournament.cs
+++ Kandanda/Kandanda.Dal/Entities/Tournament.cs
@@ -17,7 +17,38 @@ namespace Kandanda.Dal.Entities
 
         [Column(TypeName = "datetime2")]
         public DateTime Until { get; set; }
+
+        public int PublicId { get; set; }
+
+        [StringLength(255)]
+        public string SharedLink { get; set; }
         
+        public TimeSpan PlayTimeStart { get; set; }
+
+        public TimeSpan PlayTimeEnd { get; set; }
+        
+        public TimeSpan GameDuration { get; set; }
+
+        public TimeSpan BreakBetweenGames { get; set; }
+
+        public TimeSpan LunchBreakStart { get; set; }
+
+        public TimeSpan LunchBreakEnd { get; set; }
+
+        public bool Monday { get; set; }
+
+        public bool Tuesday { get; set; }
+
+        public bool Wednesday { get; set; }
+
+        public bool Thursday { get; set; }
+
+        public bool Friday { get; set; }
+
+        public bool Saturday { get; set; }
+
+        public bool Sunday { get; set; }
+
         public int OrganizerId { get; set; }
 
         [Timestamp]
