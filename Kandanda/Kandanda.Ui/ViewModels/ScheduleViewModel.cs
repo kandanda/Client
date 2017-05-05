a9cb88648982ff027008517b408a5e237464c352
kasacka <joel.egger@hsr.ch>
2017-05-05 09:03:17 +0200

    schedule renamed

--- Kandanda/Kandanda.Ui/ViewModels/ScheduleViewModel.cs
+++ Kandanda/Kandanda.Ui/ViewModels/ScheduleViewModel.cs
@@ -9,13 +9,13 @@ using Prism.Regions;
 
 namespace Kandanda.Ui.ViewModels
 {
-    public class SheduleViewModel : TournamentViewModelBase, INavigationAware
+    public class ScheduleViewModel : TournamentViewModelBase, INavigationAware
     {
         private readonly IPhaseService _phaseService;
         private Phase _currentPhase;
         public ICommand SaveCommand { get; set; }
 
-        public SheduleViewModel(IPhaseService phaseService)
+        public ScheduleViewModel(IPhaseService phaseService)
         {
             Title = "Shedules";
             _phaseService = phaseService;
