c09fd44da516223522136594d76df5b2c0a4a5c9
Victor Ruch <victor.ruch@hsr.ch>
2017-05-12 14:25:36 +0200

    Implementing logging in Prism. Adding application wide error logging.

---
+++ Kandanda/Kandanda.BusinessLayer/FileLogger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Logging;
+
+namespace Kandanda.BusinessLayer
+{
+    public class FileLogger: ILoggerFacade
+    {
+        private readonly string _path;
+
+        public FileLogger (string path)
+        {
+            _path = path;
+            CreateDirectoryIfNotExists();
+        }
+
+        private void CreateDirectoryIfNotExists()
+        {
+            if (_path != null) Directory.CreateDirectory(Path.GetDirectoryName(_path));
+        }
+
+        public void Log(string message, Category category, Priority priority)
+        {
+            using (var f = File.AppendText(_path))
+            {
+                f.WriteLine("{0} {1} {2}: {3}", DateTime.Now, category, priority, message);
+            }
+        }
+    }
+}
