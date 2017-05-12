180a1b620b251f20ed138bccc8bc47c935d48592
Janic Mikes <janic.mikes@hsr.ch>
2017-05-12 08:58:19 +0200

    Bound the delete button with command It observes changes on the CurrentTournament automatically to show the acurate state.

--- Kandanda/Kandanda.Ui/Core/TournamentViewModelBase.cs
+++ Kandanda/Kandanda.Ui/Core/TournamentViewModelBase.cs
@@ -6,16 +6,13 @@ namespace Kandanda.Ui.Core
     public abstract class TournamentViewModelBase: ViewModelBase
     {
         private Tournament _currentTournament;
-        
-        public DelegateCommand DeleteTournamentCommand { get; set; }
-        
+       
         public virtual Tournament CurrentTournament
         {
             get { return _currentTournament; }
             set
             {
                 SetProperty(ref _currentTournament, value);
-                DeleteTournamentCommand?.RaiseCanExecuteChanged();
             }
         }
     }
